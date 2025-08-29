bc74cf47519b88a195652647926dc6f4c0d6c529
Ty Christensen <tychristensen97@gmail.com>
2025-08-28 22:00:40 -0700

    added magefile

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/tydanny/foodwheel-api
 
 go 1.24.6
+
+require (
+	github.com/magefile/mage v1.15.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20250826171959-ef028d996bc1
+	google.golang.org/protobuf v1.36.8
+)
+
+require github.com/google/go-cmp v0.7.0 // indirect
---
+++ magefile.go
@@ -0,0 +1,117 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+func Lint() {
+	mg.Deps(BufLint, APILint)
+}
+
+func APILint() error {
+	mg.Deps(InstallAPILinter())
+	return sh.RunV(apiLinter())
+}
+
+func BufLint() error {
+	mg.Deps(InstallBuf)
+	return sh.RunV(buf(), "lint")
+}
+
+// Generate generates the golang files for all the protobuf apis.
+func Generate() error {
+	mg.Deps(InstallBuf)
+
+	err := sh.Run(buf(), "dep", "update")
+	if err != nil {
+		return fmt.Errorf("failed to update protobuf dependencies: %w", err)
+	}
+
+	err = sh.Run(buf(), "generate")
+	if err != nil {
+		return fmt.Errorf("failed to generate golang files: %w", err)
+	}
+
+	return nil
+}
+
+var localBin string
+
+func LocalBin() error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get working dir: %w", err)
+	}
+
+	localBin = wd + "/bin"
+
+	err = os.MkdirAll(localBin, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to create local bin dir: %w", err)
+	}
+
+	return nil
+}
+
+// Bin installs binaries needed for generation and validation.
+func Bin() {
+	fmt.Println("Installing Deps...")
+
+	mg.Deps(LocalBin, InstallBuf)
+}
+
+func goInstall(repo, version string) error {
+	return sh.RunWith(map[string]string{"GOBIN": localBin}, "go", "install", repo+version)
+}
+
+const bufVersion = "1.57.0"
+
+func buf() string {
+	return localBin + "/buf-" + bufVersion
+}
+
+// InstallBuf installs the Buf CLI into the local bin.
+func InstallBuf() error {
+	mg.Deps(LocalBin)
+
+	err := goInstall("github.com/bufbuild/buf/cmd/buf@v", bufVersion)
+	if err != nil {
+		return fmt.Errorf("failed to install buf: %s", err)
+	}
+
+	err = os.Rename(localBin+"/buf", buf())
+	if err != nil {
+		return fmt.Errorf("failed to rename buf binary: %w", err)
+	}
+
+	return nil
+}
+
+const aipLintVersion = "1.70.2"
+
+func apiLinter() string {
+	return localBin + "/api-linter-" + aipLintVersion
+}
+
+func InstallAPILinter() error {
+	mg.Deps(LocalBin)
+
+	err := goInstall("github.com/googleapis/api-linter/cmd/api-linter@v", aipLintVersion)
+	if err != nil {
+		return fmt.Errorf("failed to install the API linter: %w", err)
+	}
+
+	err = os.Rename(localBin+"/api-linter", apiLinter())
+	if err != nil {
+		return fmt.Errorf("failed to rename the API linter: %w", err)
+	}
+
+	return nil
+}
